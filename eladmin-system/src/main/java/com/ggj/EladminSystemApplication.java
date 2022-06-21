bec5d20574164254dad23621ecb8eb37893f3592
郭国杰 <guogj@brains-info.com>
2022-06-21 14:20:29 +0800

    first commit

---
+++ eladmin-system/src/main/java/com/ggj/EladminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ggj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EladminSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EladminSystemApplication.class, args);
+  }
+
+}
---
+++ eladmin-system/src/test/java/com/ggj/EladminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
