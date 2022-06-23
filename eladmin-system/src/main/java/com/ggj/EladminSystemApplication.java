61bd87ae38cf4ffe1f5e958a1a71432d76669865
郭国杰 <guogj@brains-info.com>
2022-06-23 18:02:16 +0800

    添加swagger配置，以及数据库，redis配置信息

--- eladmin-system/src/main/java/com/ggj/EladminSystemApplication.java
+++ eladmin-system/src/main/java/com/ggj/EladminSystemApplication.java
@@ -1,13 +1,44 @@
 package com.ggj;
 
+import com.ggj.annotation.rest.AnonymousGetMapping;
+import com.ggj.util.SpringContextHolder;
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(hidden = true)
+@RestController
 @SpringBootApplication
 public class EladminSystemApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EladminSystemApplication.class, args);
+  }
+
+  @Bean
+  public SpringContextHolder springContextHolder() {
+    return new SpringContextHolder();
+  }
+
+  @Bean
+  public ServletWebServerFactory webServerFactory() {
+    TomcatServletWebServerFactory fa = new TomcatServletWebServerFactory();
+    fa.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "[]{}"));
+    return fa;
+  }
+
+  /**
+   * 访问首页提示
+   *
+   * @return /
+   */
+  @AnonymousGetMapping("/")
+  public String index() {
+    return "Backend service started successfully";
   }
 
 }
